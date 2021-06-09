26109b4ee14b811049c37bea1026ba46cf9ca4c4
renan <renan.assis.alves@hotmail.com>
2021-06-08 22:33:48 -0300

    Versao Inicial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3255d7774a39bc07d63fb68d10ee68034a17b718',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3255d7774a39bc07d63fb68d10ee68034a17b718',
+    ),
+  ),
+);
